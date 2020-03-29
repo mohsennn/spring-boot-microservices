76fd75b27ef1c3af6f23d83cb81955907ea6672e
GHANEM <mohsenghanem02@gmail.com>
2020-03-30 00:04:26 +0200

    Tuto 14 Circuit breaker

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,22 +1,21 @@
 package io.javabrains.moviecatalogservice.resources;
 
-
-
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
-import io.javabrains.moviecatalogservice.models.Rating;
 import io.javabrains.moviecatalogservice.models.UserRating;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/catalog")
@@ -26,22 +25,29 @@ public class MovieCatalogResource {
 	private WebClient.Builder webClientBuilder;
 	@Autowired
 	private RestTemplate restTemplate;
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@RequestMapping("/{userId}")
+	@HystrixCommand(fallbackMethod="getFallbackCatalog")
 	public List<CatalogItem> getCtatlog(@PathVariable("userId") String userId) {
 
-	/*first call to rating rest api*/
-	UserRating ratings= restTemplate.getForObject("http://localhost:8083/ratingdata/users/"+userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject(
+				"http://ratings-data-service/ratingdata/users/" + userId,
+				UserRating.class);
 
-		
-		return ratings.getUserRating().stream().map(rating-> {
-			/*second call to movie rest api		 */
-	 Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+ rating.getMovieId(), Movie.class);
-				  
-				  return new CatalogItem(movie.getName(), "DESC_hardcoded",rating.getRating());
+		return ratings
+				.getUserRating()
+				.stream()
+				.map(rating -> {
+					Movie movie = restTemplate.getForObject(
+							"http://movie-info-service/movies/"
+									+ rating.getMovieId(), Movie.class);
+
+					return new CatalogItem(movie.getName(), "DESC_hardcoded",
+							rating.getRating());
 				}).collect(Collectors.toList());
 
-		 
 	}
 
 }
--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -2,16 +2,12 @@ package io.javabrains.movieinfoservice.resources;
 
 import io.javabrains.movieinfoservice.models.Movie;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/movies")
-
 public class MovieResource {
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
